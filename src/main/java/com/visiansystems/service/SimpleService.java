1ead607fac768a1c046645d5f99ef1033d1f40e1
ferrem01 <mauricio.ferreira@bbc.co.uk>
2015-10-01 20:37:16 +0100

    Added simple repository + service [In progress].

---
+++ src/main/java/com/visiansystems/service/SimpleService.java
@@ -0,0 +1,59 @@
+package com.visiansystems.service;
+
+import com.visiansystems.model.SimpleObject;
+
+import java.util.Collection;
+
+/**
+ * The SimpleObjectService interface defines all public business behaviors for operations on the
+ * 'SimpleObject' entity model.
+ * <p/>
+ * This interface should be injected into SimpleObjectService clients, not the implementation bean.
+ */
+public interface SimpleService {
+
+    /**
+     * Find all 'SimpleObject' entities.
+     *
+     * @return A Collection of 'SimpleObject'.
+     */
+    Collection<SimpleObject> findAll();
+
+    /**
+     * Find a single Greeting entity by primary key identifier.
+     *
+     * @param id A BigInteger primary key identifier.
+     * @return A Greeting or <code>null</code> if none found.
+     */
+    SimpleObject findOne(Long id);
+
+    /**
+     * Persists a Greeting entity in the data store.
+     *
+     * @param greeting A Greeting object to be persisted.
+     * @return A persisted Greeting object or <code>null</code> if a problem
+     * occurred.
+     */
+    SimpleObject create(SimpleObject greeting);
+
+    /**
+     * Updates a previously persisted Greeting entity in the data store.
+     *
+     * @param greeting A Greeting object to be updated.
+     * @return An updated Greeting object or <code>null</code> if a problem
+     * occurred.
+     */
+    SimpleObject update(SimpleObject greeting);
+
+    /**
+     * Removes a previously persisted Greeting entity from the data store.
+     *
+     * @param id A BigInteger primary key identifier.
+     */
+    void delete(Long id);
+
+    /**
+     * Evicts all members of the "greetings" cache.
+     */
+    void evictCache();
+}
